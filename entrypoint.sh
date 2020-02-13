4871d265759d26fb79dd188e6a3416524a9f6449
b4b4r07 <b4b4r07@gmail.com>
2020-02-13 16:26:02 +0900

    Add outputs.result to store the command output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-args=()
+main() {
+  local -a args=()
 
-if ${INPUT_IMPORT:-false}; then
-  args+=("--import")
-fi
+  if ${INPUT_IMPORT:-false}; then
+    args+=("--import")
+  fi
 
-if ${INPUT_DRYRUN:-false}; then
-  args+=("--dry-run")
-fi
+  if ${INPUT_DRYRUN:-false}; then
+    args+=("--dry-run")
+  fi
 
-config=${INPUT_CONFIG}
-github-labeler --config=${config} ${args[@]}
+  local config=${INPUT_CONFIG}
+  github-labeler --config=${config} ${args[@]}
+}
+
+set -o pipefail
+
+main "$@" | tee -a result
+result="$(cat result)"
+
+# https://github.community/t5/GitHub-Actions/set-output-Truncates-Multiline-Strings/td-p/37870
+echo "::set-output name=result::${result//$'\n'/'%0A'}"
